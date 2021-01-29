14bc33b5dcf18415b5877aab65d344c7447a5331
Rafa R <rafaelonline.rr@gmail.com>
2021-01-29 17:34:22 +0700

    move server folder to the root

--- server.js
+++ server.js
@@ -28,3 +28,14 @@ app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+if (process.env.NODE_ENV === 'production') {
+	// Exprees will serve up production assets
+	app.use(express.static('client/build'));
+
+	// Express serve up index.html file if it doesn't recognize route
+	const path = require('path');
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
